7cb692e4b2cdf2ea5b2c208e6b1e82d5d9337a27
misaki_lzayoi <1556809662@qq.com>
2022-11-15 10:44:03 +0800

    string index out of range

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from base import grammar_tree
 from syntaxTree import drawSyntaxTree
 
 if __name__ == '__main__':
-    src = 'input.c'
+    src = 'use_undefined.c'
     with open(src, encoding='utf-8') as f:
         str = f.read()
         if Lexer(str) and GParser():
